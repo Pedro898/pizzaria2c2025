fd31af1b86aae55401ff93a9b45c4c0efd02262d
Pedro898 <rm94331@estudante.fieb.edu.br>
2025-09-16 07:45:28 -0300

    componente

---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,15 @@
+const Produtos = () => {
+    return (
+        <div>
+          <h3> Listagem de Produtos</h3>
+          <ul> 
+
+           <li> Pizza de Calabresa</li>
+           <li> Pizza de Muçarela</li>
+           <li> Pizza Dois Queijos </li> 
+          </ul>
+        </div>
+      )
+    }
+    
+    export default Produtos
